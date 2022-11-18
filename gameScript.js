69a66acfc8e21e4fe316088092f8b7204b1cacef
Lati <lati@MacBook-Pro-de-Lati.local>
2022-11-18 20:27:54 -0300

    Subo TP

--- gameScript.js
+++ gameScript.js
@@ -1,36 +1,34 @@
-let imageOne= document.querySelector('#img1');
-let imageTwo= document.querySelector('#img2');
-let imageThree= document.querySelector('#img3');
-let dropboxOne= document.querySelector(dropbox1);
-let dropboxTwo= document.querySelector(dropbox2);
-let dropboxThree= document.querySelector(dropbox3);
+//Con este arreglo sabremos qué divs están ocupados o no.
+let array =  ["", "", ""];
 
-const saveData1=(event)=>{
-    event.dataTransfer.setData("Text","../assets/imagenes/rompe1.png")
+// función para que deje soltar el elemento
+function preventAction(ev) {
+    ev.preventDefault();
 }
 
-const saveData2=(event)=>{
-    event.dataTransfer.setData("Text","../assets/imagenes/rompe2.png")
+//Función que guarda lo que agarré con el mouse
+function saveData(ev) {
+    console.log(ev)
+    ev.dataTransfer.setData("text", ev.target.id);
 }
 
-const saveData3=(event)=>{
-    event.dataTransfer.setData("Text","../assets/imagenes/rompe3.png")
-}
 
-const preventAction=(event)=>{
-    event.preventDefault();
-}
-
-// obtener info y soltarlo
-const dropData = (event)=>{
-
-   let infoImage= event.dataTransfer.getData("Text");
-   dropboxOne=`<img src="${infoImage}"/>`
-   dropboxTwo=`<img src="${infoImage}"/>`
-   dropboxThree=`<img src="${infoImage}"/>`
+function dropData(ev) {
+    //Tomo el nombre del id del div que puede ser 0, o 1 o 2. Si el arreglo en esa posición está vacío entonces puedo
+    // soltar ahí la imagen
+    if(array[parseInt(ev.target.id)]==""){
+        //con getData obtengo los datos arrastrados en el evento (ev) 
+        var data = ev.dataTransfer.getData("text");
+        //agrego al array el dato que traje (el id de la imagen arrastrada)
+        array[parseInt(ev.target.id)] = data;
+        console.log(data)
+        //agrego el elemento arrastrado al elemento soltado
+        ev.target.appendChild(document.getElementById(data));
+    
+    }
 
 //borro el dato que guarde para que pueda volver a utilizar ese tipo de dato en otra cosa que necesite
-    event.dataTransfer.clearData("Text")
+    ev.dataTransfer.clearData("text")
 }
 function reboot() {
     window.location.reload();
